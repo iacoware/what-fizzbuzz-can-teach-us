2358c590668e4adf58fb2f4b59216e0f75847b2a
Massimo Iacolare <info@massimoiacolare.it>
2019-09-27 11:54:42 +0200

    First steps to compose Fizz & Buzz

--- FizzBuzzMachine.cs
+++ FizzBuzzMachine.cs
@@ -6,12 +6,22 @@ namespace FizzBuzzKata
         {
             if (num % 3 == 0 && num % 5 == 0) 
                 return "Fizz" + "Buzz";
-            
-            if (num % 3 == 0) 
+
+            var result = "";
+            if (num % 3 == 0)
+            {
+                result += "Fizz";
                 return "Fizz";
-            
-            if (num % 5 == 0) 
+            }
+
+            if (num % 5 == 0)
+            {
+                result += "Buzz";
                 return "Buzz";
+            }
+
+            if (result != "")
+                return result;
 
             return num.ToString();
         }
