63ed9022cf7a03614949472e947b8e19177ae4a3
Massimo Iacolare <info@massimoiacolare.it>
2019-09-27 12:54:02 +0200

    Rename tests to match intent

--- FizzBuzzTest.cs
+++ FizzBuzzTest.cs
@@ -5,7 +5,7 @@ namespace FizzBuzzKata
     public class FizzBuzzTest
     {
         [Fact]
-        public void SimpleNumber()
+        public void NoMatch()
         {
             var fizzBuzz = new FizzBuzzMachine(new[]
             {
@@ -17,7 +17,7 @@ namespace FizzBuzzKata
         }
 
         [Fact]
-        public void Fizz()
+        public void SingleMatch()
         {
             var fizzBuzz = new FizzBuzzMachine(new[]
             {
@@ -29,19 +29,7 @@ namespace FizzBuzzKata
         }
 
         [Fact]
-        public void Buzz()
-        {
-            var fizzBuzz = new FizzBuzzMachine(new[]
-            {
-                new DivisorAndWord(3, "Fizz"),
-                new DivisorAndWord(5, "Buzz")
-            });
-            Assert.Equal("Buzz", fizzBuzz.Say(5));
-            Assert.Equal("Buzz", fizzBuzz.Say(5*2));
-        }
-
-        [Fact]
-        public void FizzBuzz()
+        public void MultipleMatch()
         {
             var fizzBuzz = new FizzBuzzMachine(new[]
             {
