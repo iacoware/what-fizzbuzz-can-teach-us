cc8bff19559c56e304b86a682bcdd3cc1da14b4a
Massimo Iacolare <info@massimoiacolare.it>
2019-09-27 11:50:05 +0200

    Evident data on Fizz + Buzz

--- FizzBuzzMachine.cs
+++ FizzBuzzMachine.cs
@@ -5,7 +5,7 @@ namespace FizzBuzzKata
         public string Say(int num)
         {
             if (num % 3 == 0 && num % 5 == 0) 
-                return "FizzBuzz";
+                return "Fizz" + "Buzz";
             
             if (num % 3 == 0) 
                 return "Fizz";
